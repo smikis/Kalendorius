0dff417508d4383b037e7a0e8bf1017831d006c1
tomval <tomovaliuno@gmail.com>
2017-12-21 21:49:55 +0200

    Added calendar view in Home

--- Kalendorius/Activities/HomeActivity.cs
+++ Kalendorius/Activities/HomeActivity.cs
@@ -1,26 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System.Collections.Generic;
+using Java.Util;
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using MV = Com.Applandeo.Materialcalendarview;
 
 namespace Kalendorius.Activities
 {
     [Activity(Label = "HomeActivity", MainLauncher = true)]
-    public class HomeActivity : Activity
+    public class HomeActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Create your application here
             SetContentView(Resource.Layout.Home);
+
+            // Adding Toolbar to Main screen           
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            toolbar.Title = "Kalendorius";
+            SetSupportActionBar(toolbar);
+
+
+            MV.CalendarView calendarView = (MV.CalendarView)FindViewById(Resource.Id.calendarView);
+
+            List<MV.EventDay> events = new List<MV.EventDay>();
+
+            Calendar calendarToday = Calendar.Instance;
+            events.Add(new MV.EventDay(calendarToday, Resource.Drawable.sample_icon_1));
+            calendarView.SetEvents(events);
+
+
+            calendarView.DayClick += CalendarView_DayClick;
+
+        }
+
+        private void CalendarView_DayClick(object sender, MV.Listeners.DayClickEventArgs e)
+        {
+            
         }
     }
 }
